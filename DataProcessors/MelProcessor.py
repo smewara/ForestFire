63d16af617fb3459559de415870258b296b57b60
biancanolan <bianca.nolan11@gmail.com>
2024-04-17 11:14:05 +0200

    running Mel with CNN 3D 10 epoch

--- DataProcessors/MelProcessor.py
+++ DataProcessors/MelProcessor.py
@@ -24,7 +24,7 @@ class MelProcessor(SpectrogramProcessor):
 
         # Compute Mel for each segment
         for start_time, segment in segments:
-            mel = librosa.feature.melspectrogram(segment, n_fft=self.n_fft, hop_length=self.hop_length)
+            mel = librosa.feature.melspectrogram(y=segment, n_fft=self.n_fft, hop_length=self.hop_length)
             magnitude = np.abs(mel)
             mel_spectrogram.append((magnitude, start_time))
 
--- ForestFire.py
+++ ForestFire.py
@@ -1,40 +1,40 @@
 from Model.CNN import CNN
 from DataProcessors.Utils import Utils
-from DataProcessors.STFTProcessor import STFTProcessor
+from DataProcessors.MelProcessor import MelProcessor
 
 def main():
     # This commented block creates spectrograms. Uncomment and change spectrogram type and directory
     # to create other types of spectrograms.
-    '''
+    
     input_fire_dir = 'Data\\Pre-processed Data\\Fire'
-    stft_output_fire_dir = 'Data\\Spectrograms\\STFT\\Fire'
+    mel_output_fire_dir = 'Data\\Spectrograms\\MEL\\Fire'
      
-    Utils.process_audio_directory(spectrogram_type='STFT', 
+    Utils.process_audio_directory(spectrogram_type='MEL', 
                            input_dir=input_fire_dir, 
-                            output_dir=stft_output_fire_dir) 
+                            output_dir=mel_output_fire_dir) 
     
     input_nofire_dir = 'Data\\Pre-processed Data\\NoFire\\Environment'
-    stft_output_nofire_dir = 'Data\\Spectrograms\\STFT\\NoFire'
+    mel_output_nofire_dir = 'Data\\Spectrograms\\MEL\\NoFire'
      
-    Utils.process_audio_directory(spectrogram_type='STFT', 
+    Utils.process_audio_directory(spectrogram_type='MEL', 
                            input_dir=input_nofire_dir, 
-                            output_dir=stft_output_nofire_dir) 
-    '''
+                            output_dir=mel_output_nofire_dir) 
+    
     cnn = CNN()
     # The below code loads the spectrograms and trains CNN model. Uncomment the block to
     # train your own model and give a name to the model
 
     # Load Spectrograms
-    spectrograms, labels = Utils.load_data(input_dir='Data\\Spectrograms\\STFT')
+    spectrograms, labels = Utils.load_data(input_dir='Data\\Spectrograms\\MEL')
 
     # Train model
-    cnn.train(spectrograms=spectrograms, labels=labels, epochs=10, model_output_path='Model\\Model_STFT_10.keras')
+    cnn.train(spectrograms=spectrograms, labels=labels, epochs=1, model_output_path='Model\\Model_MEL_10.keras')
 
     # Predict on Rainforest spectrogram
-    stft = STFTProcessor()
-    spectrogram = stft.compute_spectrogram('Data\\Pre-processed Data\\NoFire\\Rainforest\\0a4e7e350_4.wav')
+    mel = MelProcessor()
+    spectrogram = mel.compute_spectrogram('Data\\Pre-processed Data\\NoFire\\Rainforest\\0a4e7e350_4.wav')
 
-    model = cnn.load_model('Model\\Model_STFT_10.keras')
+    model = cnn.load_model('Model\\Model_MEL_10.keras')
     predictions = cnn.predict(spectrogram=spectrogram, model=model)
     print(predictions)
 
