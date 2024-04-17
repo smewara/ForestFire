926640fc91eaf82dacca67183c28ffa4755b3a16
Liam Biam <124901375+liambiam@users.noreply.github.com>
2024-04-17 12:55:06 +0100

    Update Normalisation_SampleRate_Split.py

--- Normalisation_SampleRate_Split.py
+++ Normalisation_SampleRate_Split.py
@@ -5,8 +5,8 @@ import librosa
 import numpy as np
 
 # Input and output folders
-input_folder = "No Fire"
-output_folder = "No_Fire_Normalized"
+input_folder = "Original audio/No Fire sounds"
+output_folder = "Normalised audio/NOFIRE_Segmented_Normalized"
 
 # Normalises to EBU standard, splits audio into 3 second chunks, converts to 16kHz
 def normalize_audio(input_folder, output_folder, target_loudness=-23):
@@ -24,8 +24,8 @@ def normalize_audio(input_folder, output_folder, target_loudness=-23):
             
             # Convert to mono
             if audio.ndim > 1:
-                audio = librosa.to_mono(audio)
-                
+                audio = np.mean(audio, axis=1)
+            
             # Measure loudness
             meter = pyloudnorm.Meter(sample_rate)
             
@@ -35,26 +35,29 @@ def normalize_audio(input_folder, output_folder, target_loudness=-23):
             normalized_audio = pyloudnorm.normalize.loudness(audio, loudness, target_loudness)
 
             # Split audio
-            normalized_audio = split_audio_into_segments(normalized_audio, 16000)
-
+            normalized_audio = split_audio_into_segments(normalized_audio, sample_rate)
+            
             # Save segmented audio chunks
             for i, segment in enumerate(normalized_audio):
                 segment_audio, start_time = segment
                 segment_filename = f"{os.path.splitext(filename)[0]}_{i}.wav"
                 segment_output_file = os.path.join(output_folder, segment_filename)
-                sf.write(segment_output_file, segment_audio, 16000, subtype='PCM_16')
+                sf.write(segment_output_file, segment_audio, 32000, subtype='PCM_32')
 
             print(f"Normalized, converted, and segmented {filename} successfully.")
 
-def split_audio_into_segments(audio, sample_rate, duration=3, overlap=0.5):
+def split_audio_into_segments(audio, sample_rate, duration=3, overlap=0.5, min_segment_length=1.0):
     segment_samples = int(duration * sample_rate)
+    min_segment_samples = int(min_segment_length * sample_rate)
     hop_length = int(segment_samples * (1 - overlap))
+
     # Split audio into segments
     segments = []
     start_sample = 0
     while start_sample + segment_samples < len(audio):
         segment = audio[start_sample:start_sample + segment_samples]
-        segments.append((segment, start_sample / sample_rate))
+        if len(segment) >= min_segment_samples:
+            segments.append((segment, start_sample / sample_rate))
         start_sample += hop_length
 
     return segments
