efceebac91a78f962002f984eeffd94a6c596a4b
biancanolan <bianca.nolan11@gmail.com>
2024-04-25 11:22:57 +0200

    git commit -m "updating Bianca branch to merge"

--- Model/CNN.py
+++ Model/CNN.py
@@ -4,31 +4,30 @@ from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense,
 from sklearn.model_selection import train_test_split
 import numpy as np
 
-class CNN:
+class CNN_2D:
     def __init__(self, model_path = None):
         if model_path is not None:
             self.model = self.load_model(model_path=model_path)
         else:
             self.model = None
 
-    def _build_model(self, input_shape, no_of_layers=2):
+    def _build_model(self, input_shape):
         # Define input layer with the specified input shape
         inputs = Input(shape=input_shape)
 
-        x = inputs
-        filter = 32
-
         # Convolutional layers
-        for i in range(1, no_of_layers + 1):
-            x = Conv2D(filter, (3, 3), activation='relu')(x)
-            x = MaxPooling2D((2, 2), padding='same')(x)
-            filter = filter*2
+        x = Conv2D(32, (3, 3), activation='relu')(inputs)
+        x = MaxPooling2D((2, 2), padding='same')(x)
+        x = Conv2D(64, (3, 3), activation='relu')(x)
+        x = MaxPooling2D((2, 2), padding='same')(x)
+        x = Conv2D(128, (3, 3), activation='relu')(x)
+        x = MaxPooling2D((2, 2), padding='same')(x)
 
         # Flatten layer to prepare for fully connected layers
         x = Flatten()(x)
 
         # Fully connected layers (dense layers)
-        x = Dense(filter, activation='relu')(x)
+        x = Dense(128, activation='relu')(x)
         
         # Dropout layer to prevent overfitting
         x = Dropout(0.2)(x)
@@ -42,7 +41,7 @@ class CNN:
         # Compile the model
         self.model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy', 'precision', 'recall'])
 
-    def train(self, spectrograms, labels, no_of_layers, epochs, model_output_path):
+    def train(self, spectrograms, labels, epochs, model_output_path):
         # Shuffle data indices
         indices = np.arange(len(spectrograms))
         np.random.shuffle(indices)
@@ -73,7 +72,7 @@ class CNN:
         input_shape = X_train.shape[1:]
 
         # Build the model
-        self._build_model(input_shape=input_shape, no_of_layers=no_of_layers)  
+        self._build_model(input_shape)  
 
         # Train the model
         self.model.fit(X_train, y_train, epochs=epochs, batch_size=32, validation_data=(X_val, y_val))
@@ -89,8 +88,8 @@ class CNN:
         return tf.keras.models.load_model(model_path)
     
     def preprocess_spectrogram(self, spectrogram):
-        # Resize and reshape the spectrogram to match model input shape (1025, 79, 1)
-        resized_spectrogram = spectrogram[:, :79]  # Keep only the first 79 frames
+        # Resize and reshape the spectrogram to match model input shape (1025, 130, 1)
+        resized_spectrogram = spectrogram[:, :130]  # Keep only the first 130 frames
         reshaped_spectrogram = resized_spectrogram[..., np.newaxis]  # Add channel dimension
         return reshaped_spectrogram
     
