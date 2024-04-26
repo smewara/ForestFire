b6165512e232d5a98b4954f67dd92a9e0a62e334
biancanolan <bianca.nolan11@gmail.com>
2024-04-26 12:10:01 +0200

    getting it to wotk with Mel

--- ForestFire.py
+++ ForestFire.py
@@ -1,53 +1,95 @@
+import numpy as np
 from DataProcessors.Utils import Utils
 from Model.CNN import CNN
 from Model.TestMetrics import TestMetrics
 
+def save_spectrogram(spectrogram_type, input_directory, output_directory):
+    Utils.process_audio_directory(spectrogram_type=spectrogram_type, 
+                                  input_dir=input_directory, 
+                                  output_dir=output_directory,
+                                  save=True) 
+    
 def generateAndSaveSpectrograms(spectrogram_type):
     # This commented block creates spectrograms. Uncomment and change spectrogram type and directory
-    # to create other types of spectrograms.
-    input_fire_dir = 'Data\\Pre-processed Data\\Fire'
-    mfcc_output_fire_dir = f'Data\\Spectrograms\\{spectrogram_type}\\Fire'
-     
-    Utils.process_audio_directory(spectrogram_type=spectrogram_type, 
-                            input_dir=input_fire_dir, 
-                            output_dir=mfcc_output_fire_dir) 
+    # to create other types of spectrograms. 
+    save_spectrogram(spectrogram_type=spectrogram_type,
+                     input_directory='Data\\Pre-processed Data\\Train\\Fire\\Original',
+                     output_directory=f'Data\\Spectrograms\\{spectrogram_type}\\Fire')
     
-    input_nofire_dir = 'Data\\Pre-processed Data\\NoFire\\Environment'
-    mfcc_output_nofire_dir = f'Data\\Spectrograms\\{spectrogram_type}\\NoFire'
-     
-    Utils.process_audio_directory(spectrogram_type=spectrogram_type, 
-                            input_dir=input_nofire_dir, 
-                            output_dir=mfcc_output_nofire_dir) 
+    save_spectrogram(spectrogram_type=spectrogram_type,
+                     input_directory='Data\\Pre-processed Data\\Train\\Fire\\Augmented\\PitchShifted',
+                     output_directory=f'Data\\Spectrograms\\{spectrogram_type}\\Fire')
     
-def trainCNNModel(spectrogram_type, no_epochs):
+    save_spectrogram(spectrogram_type=spectrogram_type,
+                     input_directory='Data\\Pre-processed Data\\Train\\Fire\\Augmented\\TimeStretched',
+                     output_directory=f'Data\\Spectrograms\\{spectrogram_type}\\Fire')
+    
+    save_spectrogram(spectrogram_type=spectrogram_type,
+                     input_directory='Data\\Pre-processed Data\\Train\\Fire\\Augmented\\Superimposed',
+                     output_directory=f'Data\\Spectrograms\\{spectrogram_type}\\Fire')
+
+    save_spectrogram(spectrogram_type=spectrogram_type,
+                     input_directory='Data\\Pre-processed Data\\Train\\NoFire\\Environment',
+                     output_directory=f'Data\\Spectrograms\\{spectrogram_type}\\NoFire')
+    
+    save_spectrogram(spectrogram_type=spectrogram_type,
+                     input_directory='Data\\Pre-processed Data\\Train\\NoFire\\Rainforest',
+                     output_directory=f'Data\\Spectrograms\\{spectrogram_type}\\NoFire')
+    
+def trainCNNModel(spectrogram_type, no_of_layers, no_epochs):
     # The below code loads the spectrograms and trains CNN model. Uncomment the block to
     # train your own model and give a name to the model
-    cnn = CNN_2D()
+    cnn = CNN()
     # Load Spectrograms
     spectrograms, labels = Utils.load_data(input_dir=f'Data\\Spectrograms\\{spectrogram_type}')
 
     # Train model
-    cnn.train(spectrograms=spectrograms, labels=labels, epochs=no_epochs, model_output_path=f'Model\\Model_{spectrogram_type}_{no_epochs}.keras')
+    cnn.train(spectrograms=spectrograms, labels=labels, no_of_layers=no_of_layers, epochs=no_epochs, model_output_path=f'Model\\Model_{no_of_layers}D_{spectrogram_type}_{no_epochs}.keras')
+
+def get_test_spectrograms_and_labels(spectrogram_type, ):
+     # Define base folder for test data
+    base_folder = 'Data//Pre-processed Data//Test//'
+    true_labels = []
+    true_labels_no_fire = []
+    true_labels_fire = []
+    processor = Utils.get_data_processor(spectrogram_type=spectrogram_type)
+    
+    # Process 'no-fire' (Rainforest) spectrograms
+    spectrograms = Utils.process_audio_directory(spectrogram_type=spectrogram_type,
+                                                input_dir=f'{base_folder}NoFire//Rainforest',
+                                                duration_in_sec=2.5,
+                                                output_dir=None,  
+                                                save=False)  
+    true_labels.extend(['no-fire'] * len(spectrograms))
+   
+    # Process 'fire' (Youtube fire) spectrograms
+    spectrograms_fire = Utils.process_audio_directory(spectrogram_type=spectrogram_type,
+                                                      input_dir=f'{base_folder}Fire',
+                                                      duration_in_sec=30,
+                                                      output_dir=None,  
+                                                      save=False) 
+    true_labels.extend(['fire'] * len(spectrograms_fire))
+    spectrograms.extend(spectrograms_fire)
+    return spectrograms, true_labels
 
 def testModel(spectrogram_type, model_path):
-    # Testing and Printing Test Metrics
-    # Predict on Rainforest spectrogram
     testMetrics = TestMetrics(model_path=model_path, spectrogram_type=spectrogram_type)
-    testMetrics.PrintTestMetrics(audio_path = r'Data\Pre-processed Data\NoFire\Rainforest\0a4e7e350_28.wav',
-                                 trueLabel= 'no-fire',
-                                 doSegmentation=False)
-    
-    # Predict on Test Fire
-    Utils.convert_m4a_to_wav(r'Data\Pre-processed Data\Fire\Test\2HFire.m4a', r'Data\Pre-processed Data\Fire\Test\2HFire.wav')
-    testMetrics.PrintTestMetrics(audio_path = r'Data\Pre-processed Data\Fire\Test\2HFire.wav',
-                                 trueLabel= 'fire',
-                                 doSegmentation=True)
+    spectrograms, true_labels = get_test_spectrograms_and_labels(spectrogram_type=spectrogram_type)
+    # Evaluate the model using test metrics
+    testMetrics.PrintTestMetrics(spectrograms=spectrograms, true_labels=true_labels)
+
+def print_model_summary(model_path):
+    model = CNN().load_model(model_path=model_path)
+    print('{model_path}\n')
+    print(model.summary())
     
 def main():
-    spectrogram_type = 'STFT'
-    #generateAndSaveSpectrograms(spectrogram_type = spectrogram_type)
-    #trainCNNModel(spectrogram_type=spectrogram_type, no_epochs=10)
-    testModel(spectrogram_type=spectrogram_type, model_path='Model\Model_STFT_10.keras')
+    spectrogram_type = 'mel'
+    model_path = 'Model\Model_2D_MEL_10.keras'
+    generateAndSaveSpectrograms(spectrogram_type=spectrogram_type)
+    trainCNNModel(spectrogram_type=spectrogram_type, no_of_layers=2, no_epochs=10)
+    testModel(spectrogram_type=spectrogram_type, model_path='Model\Model_2D_MEL_10.keras')
+    print_model_summary(model_path)
 
 if __name__ == "__main__":
     main()
